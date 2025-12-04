d851c50711242bb245e222aa350f3bf952ec8da3
Neth Tithyarita <neth.tithyarita12345@gmail.com>
2025-12-04 13:12:48 +0700

    new-commit

--- khmer-food/src/views/Meats.vue
+++ khmer-food/src/views/Meats.vue
@@ -22,78 +22,6 @@ import Banner from '../components/Banner.vue';
 import ProductCard from '../components/ProductCard.vue';
 import { useCartStore } from '../stores/cart';
 
-<<<<<<< HEAD
-export default {
-  components: { Banner, ProductCard },
-  data() {
-    return {
-      products: [
-        { id: 1,
-          name: 'Chicken Chick',
-          price: 3,
-          inStock: true,
-          rating: 4,
-          image: '/images/chicken_chick.png' },
-
-        { id: 2,
-          name: 'Beef',
-          price: 2, inStock: false,
-          rating: 3,
-          image: '/images/beef.png' },
-
-        { id: 3,
-          name: 'Pork',
-          price: 5,
-          inStock: true,
-          rating: 5,
-          image: '/images/pork.png' },
-
-        { id: 4,
-          name: 'Lamb',
-          price: 5,
-          inStock: true,
-          rating: 5,
-          image: '/images/lamb.png' },
-
-        { id: 5,
-          name: 'Chicken Wings',
-          price: 5,
-          inStock: true,
-          rating: 5,
-          image: '/images/chicken_wing.png' },
-
-        { id: 6,
-          name: 'Chicken Drumsticks',
-          price: 5,
-          inStock: true,
-          rating: 5,
-          image: '/images/chicken_dumpstick.png' },
-
-        { id: 7,
-          name: 'Fish',
-          price: 5,
-          inStock: true,
-          rating: 5,
-          image: '/images/fish.png' },
-
-        { id: 8,
-          name: 'Salmon',
-          price: 5,
-          inStock: true,
-          rating: 5,
-          image: '/images/salmon.png' },
-
-        { id: 9,
-          name: 'Eggs',
-          price: 5,
-          inStock: true,
-          rating: 5,
-          image: '/images/egg.png' }
-
-      ]
-    }
-  }
-=======
 const cart = useCartStore();
 
 const products = [
@@ -110,7 +38,6 @@ const products = [
 
 function addToCart(product: any) {
   cart.addItem(product);
->>>>>>> 0672c9599d6598e65b67537f4dc5576599d583df
 }
 </script>
 
